06b0504f3006d618418544000fdaeb8bf2ce23f6
Surja11 <stha.surja123@gmail.com>
2025-04-17 20:04:54 +0545

    updated Admin panel

--- Blog/Areas/Admin/Controllers/UserController.cs
+++ Blog/Areas/Admin/Controllers/UserController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Specialized;
 using AspNetCoreHero.ToastNotification.Abstractions;
 using Blog.Models;
+using Blog.Utilities;
 using Blog.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,9 +24,26 @@ namespace Blog.Areas.Admin.Controllers
             _signInManager = signInManager;
             _notification = notification;
         }
-        public IActionResult Index()
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var users = await _userManager.Users.ToListAsync();
+            var vm = users.Select(x => new UserVM() {
+                Id = x.Id,
+            FirstName = x.FirstName,
+            LastName = x.LastName,
+            UserName = x.UserName,
+            Email = x.Email}).ToList();
+            foreach(var user in vm)
+            {
+                var singleUser = await _userManager.FindByIdAsync(user.Id);
+                var role = await _userManager.GetRolesAsync(singleUser);
+                user.Roles = role.FirstOrDefault();
+            }
+
+            return View(vm); 
         }
 
 
@@ -38,6 +57,109 @@ namespace Blog.Areas.Admin.Controllers
             }
             return RedirectToAction("Index", "User", new { area = "Admin" });
         }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> ResetPassword(string id)
+        {
+            var existingUser = await _userManager.FindByIdAsync(id);
+            if(existingUser == null)
+            {
+                _notification.Error("User doesnot exists");
+                return View();
+            }
+            var vm = new ResetPasswordVM()
+            {
+                Id = existingUser.Id,
+                UserName = existingUser.UserName
+
+            };
+            return View(vm);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var existingUser = await _userManager.FindByIdAsync(vm.Id);
+            if (existingUser == null)
+            {
+                _notification.Error("User doesnot exist");
+            }
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(existingUser);
+
+            var result = await _userManager.ResetPasswordAsync(existingUser,token, vm.NewPassword);
+            if (result.Succeeded)
+            {
+                _notification.Success("Password changed successfully");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
+        }
+
+        [Authorize(Roles ="Admin")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var checkUserByEmail = await _userManager.FindByEmailAsync(vm.Email);
+            if(checkUserByEmail != null)
+            {
+                _notification.Error("Email Already Exists");
+                return View(vm);
+            }
+
+            var checkUserByUsername = await _userManager.FindByNameAsync(vm.UserName);
+            if (checkUserByUsername != null)
+            {
+                _notification.Error("Username Already Exists");
+                return View(vm);
+            }
+
+            var applicationUser = new ApplicationUser()
+            {
+                Email = vm.Email,
+                UserName = vm.UserName,
+                FirstName = vm.FirstName,
+                LastName = vm.LastName
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, vm.Password);
+
+            if (result.Succeeded)
+            {
+                if (vm.IsAdmin)
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, WebsiteRoles.WebsiteAdmin);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, WebsiteRoles.WebsiteAuthor);
+                }
+
+                _notification.Success("User registered successfully");
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+
+            return View(vm);
+        }
+
 
         [HttpPost("Login")]
         public async Task<IActionResult> Login(LoginVM vm)
@@ -62,7 +184,7 @@ namespace Blog.Areas.Admin.Controllers
             }
             await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, true);
             _notification.Success("Login Successful");
-            return RedirectToAction("Index", "User", new { area = "Admin" });
+            return RedirectToAction("Index", "Post", new { area = "Admin" });
                
 
             }
@@ -76,6 +198,14 @@ namespace Blog.Areas.Admin.Controllers
 
         }
 
+        [HttpGet("AccessDenied")]
+        [Authorize]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
     }
  }
 
+ 
---
+++ Blog/ViewModels/ResetPasswordVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+    public class ResetPasswordVM
+    {
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        [Required]
+        public string? NewPassword { get; set; }
+        [Required]
+        [Compare(nameof(NewPassword))]
+        public string? ConfirmPassword { get; set; }
+
+    }
+}
